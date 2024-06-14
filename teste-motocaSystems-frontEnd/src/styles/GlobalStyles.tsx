7b790f48875a6f4e3615b0efd0ba7c5424867d00
Sayd Karam <onltrabalho@gmail.com>
2024-06-14 20:46:33 -0300

    style: :art: create GlobalStyles

---
+++ teste-motocaSystems-frontEnd/src/styles/GlobalStyles.tsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyles = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-size: 16px;
+        font-family: 'Poppins', sans-serif;
+        
+    button  {
+        cursor: pointer;
+    }
+    
+    }
+`
