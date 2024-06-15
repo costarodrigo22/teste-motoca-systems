77b928a1d4791727f6e82478f6e899ec2857d41b
Sayd Karam <onltrabalho@gmail.com>
2024-06-15 18:52:06 -0300

    feat: :zap: configura lists

--- teste-motocaSystems-frontEnd/src/pages/List/styles.ts
+++ teste-motocaSystems-frontEnd/src/pages/List/styles.ts
@@ -1,3 +1,12 @@
 import styled from "styled-components"
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #2a233c;
+  height: 91vh;
+  width: 100vw;
+  /* overflow-y: hidden; */
+  overflow-x: hidden;
+`
+export const ContainerContent = styled.div``
