6de2d7e6144016205ffa8109b9fa2d5198a4791c
Sayd Karam <onltrabalho@gmail.com>
2024-06-15 02:54:18 -0300

    feat: :zap: Create edit

--- teste-motocaSystems-frontEnd/src/components/input/index.tsx
+++ teste-motocaSystems-frontEnd/src/components/input/index.tsx
@@ -1,50 +1,104 @@
-import { InputWrapper } from "./styles"
-
+import { InputWrapper } from "./styles";
 import TextField from '@mui/material/TextField';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
 
 export interface InputComponentProps {
     label: string;
     type: string;
     fullWidth?: boolean;
+    width?: string;
+    height?: string;
+    options?: { value: string; label: string }[];
 }
 
-const InputComponent: React.FC<InputComponentProps> = ({ label, type = "text", fullWidth = true, ...props }) => {
+const InputComponent: React.FC<InputComponentProps> = ({ label, type = "text", fullWidth = true, width = '100%', height = 'auto', options, ...props }) => {
     return (
         <InputWrapper>
-            <TextField
-                label={label}
-                variant="outlined"
-                fullWidth={fullWidth}
-                type={type}
-                sx={{
-                    '& .MuiOutlinedInput-root': {
-                        '& fieldset': {
-                            borderColor: '#E7E3FC',
+            {type === 'select' ? (
+                <FormControl variant="outlined" fullWidth={fullWidth} sx={{ width: width }}>
+                    <InputLabel sx={{
+                        color: '#E7E3FC', '&.Mui-focused': { color: '#E7E3FC' }
+                    }}>{label}</InputLabel>
+                    <Select
+                        label={label}
+                        sx={{
+                            '& .MuiOutlinedInput-root': {
+                                '& fieldset': {
+                                    borderColor: '#E7E3FC',
+                                },
+                                '&:hover fieldset': {
+                                    borderColor: '#E7E3FC',
+                                },
+                                '&.Mui-focused fieldset': {
+                                    borderColor: '#E7E3FC',
+                                },
+                            },
+                            '& .MuiSelect-icon': {
+                                color: '#E7E3FC',
+                            },
+                            '& .MuiInputBase-input': {
+                                color: '#E7E3FC',
+                            },
+                            '& .MuiInputLabel-root': {
+                                color: '#E7E3FC',
+                            },
+                            '& .MuiInputLabel-outlined': {
+                                transform: 'translate(14px, 14px) scale(1)',
+                                '&.Mui-focused': {
+                                    transform: 'translate(14px, -6px) scale(0.75)',
+                                },
+                            },
+                        }}
+                        {...props}
+                    >
+                        {options?.map((option) => (
+                            <MenuItem key={option.value} value={option.value}>
+                                {option.label}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+            ) : (
+                <TextField
+                    label={label}
+                    variant="outlined"
+                    fullWidth={fullWidth}
+                    type={type}
+                    sx={{
+                        width: width,
+                        '& .MuiOutlinedInput-root': {
+                            height: height,
+                            '& fieldset': {
+                                borderColor: '#E7E3FC',
+                            },
+                            '&:hover fieldset': {
+                                borderColor: '#E7E3FC',
+                            },
+                            '&.Mui-focused fieldset': {
+                                borderColor: '#E7E3FC',
+                            },
                         },
-                        '&:hover fieldset': {
-                            borderColor: '#E7E3FC',
+                        '& .MuiInputLabel-root': {
+                            color: '#E7E3FC',
                         },
-                        '&.Mui-focused fieldset': {
-                            borderColor: '#E7E3FC',
+                        '& .MuiInputLabel-root.Mui-focused': {
+                            color: '#E7E3FC',
                         },
-                    },
-                    '& .MuiInputLabel-root': {
-                        color: '#E7E3FC',
-                    },
-                    '& .MuiInputLabel-root.Mui-focused': {
-                        color: '#E7E3FC',
-                    },
-                    '& .MuiOutlinedInput-input': {
-                        color: '##E7E3FC',
-                    }, '& .MuiInputLabel-input': {
-                        color: '##E7E3FC',
-                    }, 'input': {
-                        color: '#E7E3FC',
-                    }
-                }}
-                {...props}
-            />
-        </InputWrapper>
+                        '& .MuiOutlinedInput-input': {
+                            color: '#E7E3FC',
+                        },
+                        'input': {
+                            color: '#E7E3FC',
+                        }
+                    }}
+                    {...props}
+                />
+            )
+            }
+        </InputWrapper >
     );
 }
 
--- teste-motocaSystems-frontEnd/src/pages/Edit/styles.ts
+++ teste-motocaSystems-frontEnd/src/pages/Edit/styles.ts
@@ -1,1 +1,49 @@
-// import styled from "styled-components"
+import styled from "styled-components"
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #2a233c !important;
+  height: 91vh !important;
+  overflow-y: hidden;
+
+  h1 {
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 19px;
+  }
+`
+export const ContainerControler = styled.form``
+
+export const ContainerContent = styled.div`
+  h1 {
+    margin-bottom: 3%;
+  }
+`
+export const Space = styled.div`
+  margin: 4px;
+`
+
+export const Form = styled.form`
+  margin-left: 35%;
+  margin-right: 35%;
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 381px) and (max-width: 559px) {
+    margin-left: 20%;
+    margin-right: 20%;
+  }
+  @media (min-width: 560px) and (max-width: px) {
+    margin-left: 25%;
+    margin-right: 25%;
+  }
+
+  @media (min-width: 150px) and (max-width: 380px) {
+    margin-left: 15%;
+    margin-right: 15%;
+  }
+`
