948fb631b069b1a9b79044c36a10ec42759e596c
Sayd Karam <onltrabalho@gmail.com>
2024-06-15 19:11:57 -0300

    fix: :zap: change all components

--- teste-motocaSystems-frontEnd/src/components/input/index.tsx
+++ teste-motocaSystems-frontEnd/src/components/input/index.tsx
@@ -1,57 +1,24 @@
-import { InputWrapper } from "./styles";
-import TextField from '@mui/material/TextField';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
+import { StyledFormControl, InputWrapper, StyledInputLabel, StyledSelect, StyledTextField } from "./styles"
 
-export interface InputComponentProps {
+import MenuItem from '@mui/material/MenuItem';
+
+interface InputComponentProps {
     label: string;
-    type: string;
+    type?: string;
     fullWidth?: boolean;
-    width?: string;
-    height?: string;
     options?: { value: string; label: string }[];
 }
 
-const InputComponent: React.FC<InputComponentProps> = ({ label, type = "text", fullWidth = true, width = '100%', height = 'auto', options, ...props }) => {
+const InputComponent: React.FC<InputComponentProps> = ({ label, type = "text", fullWidth = true, options, ...props }) => {
     return (
         <InputWrapper>
             {type === 'select' ? (
-                <FormControl variant="outlined" fullWidth={fullWidth} sx={{ width: width }}>
-                    <InputLabel sx={{
-                        color: '#E7E3FC', '&.Mui-focused': { color: '#E7E3FC' }
-                    }}>{label}</InputLabel>
-                    <Select
+                <StyledFormControl variant="outlined" fullWidth={fullWidth} >
+                    <StyledInputLabel>
+                        {label}
+                    </StyledInputLabel>
+                    <StyledSelect
                         label={label}
-                        sx={{
-                            '& .MuiOutlinedInput-root': {
-                                '& fieldset': {
-                                    borderColor: '#E7E3FC',
-                                },
-                                '&:hover fieldset': {
-                                    borderColor: '#E7E3FC',
-                                },
-                                '&.Mui-focused fieldset': {
-                                    borderColor: '#E7E3FC',
-                                },
-                            },
-                            '& .MuiSelect-icon': {
-                                color: '#E7E3FC',
-                            },
-                            '& .MuiInputBase-input': {
-                                color: '#E7E3FC',
-                            },
-                            '& .MuiInputLabel-root': {
-                                color: '#E7E3FC',
-                            },
-                            '& .MuiInputLabel-outlined': {
-                                transform: 'translate(14px, 14px) scale(1)',
-                                '&.Mui-focused': {
-                                    transform: 'translate(14px, -6px) scale(0.75)',
-                                },
-                            },
-                        }}
                         {...props}
                     >
                         {options?.map((option) => (
@@ -59,47 +26,19 @@ const InputComponent: React.FC<InputComponentProps> = ({ label, type = "text", f
                                 {option.label}
                             </MenuItem>
                         ))}
-                    </Select>
-                </FormControl>
+                    </StyledSelect>
+                </StyledFormControl>
             ) : (
-                <TextField
+                <StyledTextField
                     label={label}
                     variant="outlined"
                     fullWidth={fullWidth}
                     type={type}
-                    sx={{
-                        width: width,
-                        '& .MuiOutlinedInput-root': {
-                            height: height,
-                            '& fieldset': {
-                                borderColor: '#E7E3FC',
-                            },
-                            '&:hover fieldset': {
-                                borderColor: '#E7E3FC',
-                            },
-                            '&.Mui-focused fieldset': {
-                                borderColor: '#E7E3FC',
-                            },
-                        },
-                        '& .MuiInputLabel-root': {
-                            color: '#E7E3FC',
-                        },
-                        '& .MuiInputLabel-root.Mui-focused': {
-                            color: '#E7E3FC',
-                        },
-                        '& .MuiOutlinedInput-input': {
-                            color: '#E7E3FC',
-                        },
-                        'input': {
-                            color: '#E7E3FC',
-                        }
-                    }}
                     {...props}
                 />
-            )
-            }
-        </InputWrapper >
+            )}
+        </InputWrapper>
     );
-}
+};
 
 export default InputComponent;
--- teste-motocaSystems-frontEnd/src/components/input/styles.ts
+++ teste-motocaSystems-frontEnd/src/components/input/styles.ts
@@ -1,5 +1,71 @@
-import { styled } from "@mui/system"
+import styled from "styled-components"
 
-export const InputWrapper = styled("div")({
-  marginBottom: "10px",
-})
+import TextField from "@mui/material/TextField"
+import Select from "@mui/material/Select"
+import InputLabel from "@mui/material/InputLabel"
+import FormControl from "@mui/material/FormControl"
+
+export const InputWrapper = styled.div`
+  margin-bottom: 16px;
+`
+
+export const StyledFormControl = styled(FormControl)``
+
+export const StyledInputLabel = styled(InputLabel)`
+  color: #e7e3fc;
+
+  &.Mui-focused {
+    color: #e7e3fc;
+  }
+`
+export const StyledSelect = styled(Select)`
+  & .MuiOutlinedInput-root {
+    & fieldset {
+      border-color: #e7e3fc;
+    }
+    &:hover fieldset {
+      border-color: #e7e3fc;
+    }
+    &.Mui-focused fieldset {
+      border-color: #e7e3fc;
+    }
+  }
+  & .MuiSelect-icon {
+    color: #e7e3fc;
+  }
+  & .MuiInputBase-input {
+    color: #e7e3fc;
+  }
+  & .MuiInputLabel-root {
+    color: #e7e3fc;
+  }
+  & .MuiInputLabel-outlined {
+    transform: translate(14px, 14px) scale(1);
+    &.Mui-focused {
+      transform: translate(14px, -6px) scale(0.75);
+    }
+  }
+`
+
+export const StyledTextField = styled(TextField)`
+  & .MuiOutlinedInput-root {
+    & fieldset {
+      border-color: #e7e3fc;
+    }
+    &:hover fieldset {
+      border-color: #e7e3fc;
+    }
+    &.Mui-focused fieldset {
+      border-color: #e7e3fc;
+    }
+  }
+  & .MuiInputLabel-root {
+    color: #e7e3fc;
+  }
+  & .MuiInputLabel-root.Mui-focused {
+    color: #e7e3fc;
+  }
+  & .MuiOutlinedInput-input {
+    color: #e7e3fc;
+  }
+`
