fec5e663248efe048d778a0f730d74c790cd1bb5
Sayd Karam <onltrabalho@gmail.com>
2024-06-15 00:37:18 -0300

    style: :zap: finish layout register

--- teste-motocaSystems-frontEnd/src/pages/Register/styles.ts
+++ teste-motocaSystems-frontEnd/src/pages/Register/styles.ts
@@ -1,1 +1,35 @@
-// import styled from "styled-components"
+import styled from "styled-components"
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #2a233c !important;
+  height: 91vh !important;
+  overflow-y: hidden;
+
+  h1 {
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 19px;
+  }
+`
+export const ContainerControler = styled.form``
+
+export const ContainerContent = styled.div`
+  h1 {
+    margin-bottom: 3%;
+  }
+`
+export const Space = styled.div`
+  margin: 4px;
+`
+
+export const Form = styled.form`
+  margin-left: 35%;
+  margin-right: 35%;
+  display: flex;
+  flex-direction: column;
+`
