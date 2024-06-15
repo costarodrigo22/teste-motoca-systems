18a3e4ffd8aba672dacc4c2850853d3ddfd753e2
Sayd Karam <onltrabalho@gmail.com>
2024-06-15 19:11:39 -0300

    style: :art: fix some layouts

---
+++ teste-motocaSystems-frontEnd/src/components/header/styles.ts
@@ -0,0 +1,14 @@
+import { styled } from "styled-components"
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 100vw;
+  height: 9vh;
+
+  background: #2a233c !important;
+`
+
+export const Space = styled.div`
+  margin-left: 2%;
+`
